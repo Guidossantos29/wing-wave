8b6c361ff8bd0eb17f8a56b5adfa90689d5e80ff
Luiz Guilherme <lguidossantos29@gmail.com>
2024-01-25 23:16:02 -0300

    feat: created validation  Authentication

--- backend/routers/UserRoutes.js
+++ backend/routers/UserRoutes.js
@@ -2,15 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 
-const { register,login } = require('../controllers/UserController')
+const { register,login, getCurrentUser } = require('../controllers/UserController')
 
 
 const validate = require('../middlewares/handleValidation')
 
 const {userCreateValidation,loginValidation} = require('../middlewares/userValidation')
 
+const authGuard = require('../middlewares/authGuard')
+
 router.post('/register', userCreateValidation() ,validate, register)
 router.post('/login', loginValidation() ,validate, login)
+router.get('/profile',authGuard,getCurrentUser)
 
 
 
